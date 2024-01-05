b3edba1bbeca647e45ff8309c1ff88615821ffcd
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2024-01-05 14:29:23 +0200

    Add lethalconfig support

--- Patches/WalkieTalkiePatch.cs
+++ Patches/WalkieTalkiePatch.cs
@@ -5,22 +5,20 @@ namespace WalkieVolume.Patches
     [HarmonyPatch(typeof(WalkieTalkie))]
     internal class WalkieTalkiePatch
     {
-        private static readonly float DEFAULT_VOLUME = 1f;
-
         [HarmonyPatch("OnEnable")]
         [HarmonyPrefix]
         private static void PatchVolume(ref float ___maxVolume)
         {
             // Set held walkie talkie volume to configured value
-            ___maxVolume = DEFAULT_VOLUME * WalkieVolumeBase.VolumeMultiplier.Value;
+            ___maxVolume = WalkieVolumeBase.WalkieVolume.Value;
             WalkieVolumeBase.mls.LogInfo($"Volume set to value {___maxVolume}.");
 
             // Get list of all walkie talkies and set their volume to the configured value using reflection
             WalkieTalkie.allWalkieTalkies.ForEach(t =>
             {
                 var prop = t.GetType().GetField("maxVolume", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                prop.SetValue(t, DEFAULT_VOLUME * WalkieVolumeBase.VolumeMultiplier.Value);
-                WalkieVolumeBase.mls.LogInfo($"Walkie volume set to: {DEFAULT_VOLUME * WalkieVolumeBase.VolumeMultiplier.Value}");
+                prop.SetValue(t, WalkieVolumeBase.WalkieVolume.Value);
+                WalkieVolumeBase.mls.LogInfo($"Walkie volume set to: {WalkieVolumeBase.WalkieVolume.Value}");
             });
         }
     }
--- WalkieVolumeBase.cs
+++ WalkieVolumeBase.cs
@@ -1,20 +1,23 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
+using LethalConfig;
+using LethalConfig.ConfigItems;
+using LethalConfig.ConfigItems.Options;
 using HarmonyLib;
 using WalkieVolume.Patches;
-using System;
 
 namespace WalkieVolume
 {
     [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("ainavt.lc.lethalconfig")]
     public class WalkieVolumeBase : BaseUnityPlugin
     {
         private const string modGUID = "unity.WalkieVolumeMod";
         private const string modName = "WalkieVolume";
         private const string modVersion = "1.0.0.0";
 
-        public static ConfigEntry<float> VolumeMultiplier;
+        public static ConfigEntry<float> WalkieVolume;
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
@@ -26,10 +29,19 @@ namespace WalkieVolume
         {
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
-            VolumeMultiplier = base.Config.Bind<float>("General", "VolumeMultiplier", 1f, "Volume multiplier. 0.5 for 50%, 1.5 for 150% etc. Max value is 2.");
-            VolumeMultiplier.Value = Math.Min(Math.Max(VolumeMultiplier.Value, 0f), 2f);
+            WalkieVolume = base.Config.Bind<float>("General", "WalkieVolume", 1f, "Volume multiplier. 0.5 for 50%, 1.5 for 150% etc. Default is 1. Max value is 2.");
 
-            
+            // Volume slider configuration.
+            var volumeSlider = new FloatSliderConfigItem(WalkieVolume, new FloatSliderOptions
+            {
+                RequiresRestart = false,
+                Min = 0f,
+                Max = 2f,
+            });
+            LethalConfigManager.AddConfigItem(volumeSlider);
+            LethalConfigManager.SetModDescription("Mod for adjusting walkie talkie volume.");
+
+
             if (Instance == null)
             {
                 Instance = this;
@@ -38,7 +50,7 @@ namespace WalkieVolume
             harmony.PatchAll(typeof(WalkieVolumeBase));
             harmony.PatchAll(typeof(WalkieTalkiePatch));
 
-            mls.LogInfo($"WalkieVolume mod loaded. Walkie volume set to: {VolumeMultiplier.Value * 100}%");
+            mls.LogInfo($"WalkieVolume mod loaded. Walkie volume set to: {WalkieVolume.Value * 100}%");
         }
     }
 }
