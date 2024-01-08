14ac7baf8b57875baebe6bdd0c88ead0e9e6af91
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2024-01-08 20:14:32 +0200

    Remove redundant code and update gitignore

--- Patches/WalkieTalkiePatch.cs
+++ Patches/WalkieTalkiePatch.cs
@@ -12,14 +12,6 @@ namespace WalkieVolume.Patches
             // Set held walkie talkie volume to configured value
             ___maxVolume = WalkieVolumeBase.WalkieVolume.Value;
             WalkieVolumeBase.mls.LogInfo($"Volume set to value {___maxVolume}.");
-
-            // Get list of all walkie talkies and set their volume to the configured value using reflection
-            WalkieTalkie.allWalkieTalkies.ForEach(t =>
-            {
-                var prop = t.GetType().GetField("maxVolume", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
-                prop.SetValue(t, WalkieVolumeBase.WalkieVolume.Value);
-                WalkieVolumeBase.mls.LogInfo($"Walkie volume set to: {WalkieVolumeBase.WalkieVolume.Value}");
-            });
         }
     }
 }
