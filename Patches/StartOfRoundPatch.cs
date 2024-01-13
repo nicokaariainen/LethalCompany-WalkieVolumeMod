8cff18df372170b050ea76e2063a7b20ac075712
Nico Kääriäinen <nico.kaariainen0@gmail.com>
2024-01-13 16:45:19 +0200

    Complete rewrite of the core logic
    Previous idea was too simple. Now modifying UpdatePlayerVoiceEffects inside StartOfRound instead of WalkieTalkie

---
+++ Patches/StartOfRoundPatch.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+
+namespace WalkieVolume.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundPatch
+    {
+        public const float DEFAULT_VOLUME_ALIVE = 1.0f;
+        public const float DEFAULT_VOLUME_DEAD = 0.8f;
+
+        [HarmonyPatch("UpdatePlayerVoiceEffects")]
+        [HarmonyPostfix]
+        public static void PatchUpdatePlayerVoiceEffects()
+        {
+            if (GameNetworkManager.Instance == null || GameNetworkManager.Instance.localPlayerController == null)
+            {
+                return;
+            }
+
+            var localPlayerScript = (!GameNetworkManager.Instance.localPlayerController.isPlayerDead 
+                || !(GameNetworkManager.Instance.localPlayerController.spectatedPlayerScript != null)
+                ? GameNetworkManager.Instance.localPlayerController 
+                : GameNetworkManager.Instance.localPlayerController.spectatedPlayerScript);
+
+            foreach (var playerScript in StartOfRound.Instance.allPlayerScripts)
+            {
+                if ((!playerScript.isPlayerControlled && !playerScript.isPlayerDead)
+                    || playerScript == GameNetworkManager.Instance.localPlayerController)
+                    continue;
+
+                if (playerScript.voicePlayerState == null 
+                    || playerScript.currentVoiceChatIngameSettings._playerState == null 
+                    || playerScript.currentVoiceChatAudioSource == null)
+                    continue;
+                
+                // We don't want to modify the volume if the currently looked at player is dead
+                if (playerScript.isPlayerDead) continue;
+
+                // Flag that checks whether the local player should hear the current player from a walkie talkie
+                var isHeardFromWalkie = 
+                    playerScript.speakingToWalkieTalkie 
+                    && localPlayerScript.holdingWalkieTalkie 
+                    && playerScript != localPlayerScript;
+
+                var defaultVolume = 
+                    GameNetworkManager.Instance.localPlayerController.isPlayerDead 
+                    ? DEFAULT_VOLUME_DEAD 
+                    : DEFAULT_VOLUME_ALIVE;
+
+                // Set volume according to configured setting if player heard from walkie, else set to default.
+                if (isHeardFromWalkie)
+                {
+                    playerScript.voicePlayerState.Volume = defaultVolume * WalkieVolumeBase.WalkieVolume.Value;
+                }
+            }
+        }
+
+    }
+}
--- WalkieVolumeBase.cs
+++ WalkieVolumeBase.cs
@@ -15,7 +15,7 @@ namespace WalkieVolume
     {
         private const string modGUID = "unity.WalkieVolumeMod";
         private const string modName = "WalkieVolume";
-        private const string modVersion = "1.0.0.0";
+        private const string modVersion = "1.1.0";
 
         public static ConfigEntry<float> WalkieVolume;
 
@@ -29,14 +29,14 @@ namespace WalkieVolume
         {
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
-            WalkieVolume = base.Config.Bind<float>("General", "WalkieVolume", 1f, "Volume multiplier. 0.5 for 50%, 1.5 for 150% etc. Default is 1. Max value is 2.");
+            WalkieVolume = base.Config.Bind<float>("General", "WalkieVolume", 1f, "Volume multiplier. 0.5 for 50%, 0.75 for 75% etc. Default is 100%.");
 
             // Volume slider configuration.
             var volumeSlider = new FloatSliderConfigItem(WalkieVolume, new FloatSliderOptions
             {
                 RequiresRestart = false,
                 Min = 0f,
-                Max = 2f,
+                Max = 1f,
             });
             LethalConfigManager.AddConfigItem(volumeSlider);
             LethalConfigManager.SetModDescription("Mod for adjusting walkie talkie volume.");
@@ -48,7 +48,7 @@ namespace WalkieVolume
             }
 
             harmony.PatchAll(typeof(WalkieVolumeBase));
-            harmony.PatchAll(typeof(WalkieTalkiePatch));
+            harmony.PatchAll(typeof(StartOfRoundPatch));
 
             mls.LogInfo($"WalkieVolume mod loaded. Walkie volume set to: {WalkieVolume.Value * 100}%");
         }
